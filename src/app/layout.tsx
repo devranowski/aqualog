bacb315aaf9a8c5e16604d9b72e8b5863ca0ac34
Jakub Dobranowski <jakub@dobranowski.pl>
2024-10-26 14:41:44 +0200

    feat: add Vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Analytics } from '@vercel/analytics/react';
 import localFont from 'next/font/local';
 import '../styles/globals.css';
 import '../styles/Aquarium.css';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const geistSans = localFont({
   src: '../fonts/GeistVF.woff',
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
