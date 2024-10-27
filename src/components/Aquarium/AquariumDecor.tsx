f6bd93004198309b1b032da4c616b7fccca61fbe
Jakub Dobranowski <jakub@dobranowski.pl>
2024-10-27 10:40:14 +0100

    feat: add safe bottom inset

--- src/components/Aquarium/AquariumDecor.tsx
+++ src/components/Aquarium/AquariumDecor.tsx
@@ -1,7 +1,7 @@
 import { Shell } from 'lucide-react';
 
 const AquariumDecor = () => (
-  <>
+  <div className="pb-safe absolute inset-x-0 bottom-0">
     <Shell className="absolute bottom-2 left-[18%] z-10 h-8 w-8 rotate-45 transform text-amber-200" />
     <Shell className="-rotate-15 absolute bottom-9 left-[60%] z-10 h-10 w-10 transform text-amber-300" />
     <Shell className="rotate-30 absolute bottom-5 left-[80%] z-10 h-12 w-12 transform text-amber-100" />
@@ -9,7 +9,7 @@ const AquariumDecor = () => (
     <div className="stone stone-1"></div>
     <div className="stone stone-2"></div>
     <div className="stone stone-3"></div>
-  </>
+  </div>
 );
 
 export { AquariumDecor };
--- src/components/Aquarium/AquariumPlants.tsx
+++ src/components/Aquarium/AquariumPlants.tsx
@@ -1,7 +1,7 @@
 import { Leaf, Sprout, LeafyGreen } from 'lucide-react';
 
 const AquariumPlants = () => (
-  <>
+  <div className="pb-safe absolute inset-x-0 bottom-0">
     <Leaf className="animate-sway-reverse absolute bottom-4 left-[3%] z-10 h-16 w-16 origin-bottom text-green-500" />
     <Sprout className="absolute bottom-6 left-[23%] z-10 h-20 w-20 text-yellow-400" />
     <LeafyGreen className="animate-sway absolute bottom-3 left-[32%] z-10 h-28 w-28 origin-bottom text-green-700" />
@@ -13,7 +13,7 @@ const AquariumPlants = () => (
     <Leaf className="w-18 h-18 animate-sway-reverse absolute bottom-2 left-[42%] z-10 origin-bottom text-green-300" />
     <Sprout className="w-22 h-22 absolute bottom-9 left-[73%] z-10 text-teal-500" />
     <LeafyGreen className="w-26 h-26 animate-sway absolute bottom-1 left-[65%] z-10 origin-bottom text-lime-500" />
-  </>
+  </div>
 );
 
 export { AquariumPlants };
